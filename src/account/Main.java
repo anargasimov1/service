ee6cd483ba938f37df3ae82389819765b490eb7a
anargasimov1 <anar.gasimov.89@gmail.com>
2024-01-10 18:47:04 +0400

    newa

--- src/account/Main.java
+++ src/account/Main.java
@@ -4,14 +4,16 @@ import account.entity.CatEntity;
 import account.entity.DogEntity;
 import account.entity.Entity;
 import account.entity.PersonEntity;
-
 import java.util.Scanner;
-
 
 public class Main {
     Scanner scanner = new Scanner(System.in);
-    private double num1;
-    private double num2;
+    private int num1;
+    private int num2;
+    private boolean toggle = true;
+    private int count = 0;
+    int answersTrues = 0;
+    int answersFalses = 0;
     public static void main(String[] args) {
         Main main = new Main();
         PersonEntity person = new PersonEntity("Anar",1L,32);
@@ -21,16 +23,19 @@ public class Main {
         CatEntity cat = new CatEntity("Kisa", 2L,3);
         cat.print();
         System.out.println(new Entity().hashCode());
-        main.multi();
+        while (main.toggle)main.multi();
+        System.out.println("Sizin "+ main.answersTrues+" doğru " + main.answersFalses + " yanlış cavabınız oldu!");
 
     }
     private void multi(){
-        num1 = Math.ceil(Math.random()*9);
-        num2 = Math.ceil(Math.random()*9);
-        System.out.println(num1 + "*" + num2);
+        count++;
+        if (count == 10) toggle =false;
+        num1 = (int) Math.ceil(Math.random()*9);
+        num2 = (int) Math.ceil(Math.random()*9);
 
-        double sum = scanner.nextDouble();
-        if(sum == num1*num2) System.out.println("well done!");
-        else System.out.println("wrong!");
+        System.out.print(num1 + " * " + num2 + " = ");
+        int sum = scanner.nextInt();
+        if(sum == num1*num2) answersTrues++;
+        else answersFalses++;
     }
 }
